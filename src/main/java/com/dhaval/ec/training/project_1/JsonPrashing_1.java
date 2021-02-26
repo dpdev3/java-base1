c1c519196d9761ad9e7f2ffe55428b11d206d16f
dpdev3 <dpdev3@users.noreply.github.com>
2021-02-26 15:51:05 +0530

    Initial commit

---
+++ src/main/java/com/dhaval/ec/training/project_1/JsonPrashing_1.java
@@ -0,0 +1,21 @@
+package com.dhaval.ec.training.project_1;
+
+public class JsonPrashing_1 {
+
+	public static void main(String[] args) {
+	
+		
+		JsonParsingService jsonParsingService = new JsonParsingService();
+		
+		jsonParsingService.getTodoFromWebAPI();
+		
+
+	}
+	
+	
+	
+	
+	
+	
+
+}
